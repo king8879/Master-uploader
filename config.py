70a5675e93e41d65efe06208f05e589c2cdb1762
king8879 <k4507244@gmail.com>
2025-04-25 21:29:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7690060051:AAHJOn2nJkmDBXqQCLLQLuJ1Y8ZkkOaiF1Y")  # Ensure correct key name
-    API_ID = int(os.environ.get("API_ID", "26468828"))  # Added key name and default value
-    API_HASH = os.environ.get("API_HASH", "4693513c08d1ac6af15f95b116c29478")  # Added key name for consistency
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7994904406:AAEmKvtbSe4b3UINDxWQqqnq3GE-puYpuv8")  # Ensure correct key name
+    API_ID = int(os.environ.get("API_ID", "18579024"))  # Added key name and default value
+    API_HASH = os.environ.get("API_HASH", "124981da628d86e21ee492da77cd4037")  # Added key name for consistency
 
-    AUTH_USER = os.environ.get("AUTH_USERS", "7517045929").split(',')
+    AUTH_USER = os.environ.get("AUTH_USERS", "574224129").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
 
     HOST = os.environ.get("HOST", "http://master-api-v3.vercel.app")  # Keeping HOST configurable
